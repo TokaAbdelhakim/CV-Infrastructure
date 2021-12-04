65ba652f83a02814571e201c6284db38b6775b2a
TokaAbdelhakim <lulaiola32@gmail.com>
2021-12-04 20:21:29 +0000

    create ec2 in a private subnet

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # imageowner
+}
+
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = []
+  subnet_id = aws_subnet.private_subnet.id
+
+ }
