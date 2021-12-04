bd941c3d0239a236136b112d87f31f5c78c2f2f0
TokaAbdelhakim <lulaiola32@gmail.com>
2021-12-04 16:29:42 +0000

    create vpc after authenticate as IAM user

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+Dev = "us-east-1"
+Staging = "us-east-2"
+Production = "us-west-1"
+vpc-cidr = "10.0.0.0/16"
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "Dev"{
+	type = string
+}
+
+variable "Staging"{
+	type = string
+}
+
+variable "Production"{
+	type = string
+}
+
+variable "vpc-cidr"{
+	type = string
+}
